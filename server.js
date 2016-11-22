7d4ed4c23e2941137954a1d4c1ca048a71a92c6c
Thomas Gallin <thomas.gallin@gmail.com>
2016-11-22 23:05:59 +0000

    pug template

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ var express = require('express');
 
 var app = express();
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/:timestamp', function(req, res, next) {
+    var ts = req.params.timestamp;
+    if (ts) {
+        console.log(ts);
+        res.end(ts);
+    }
+});
+
 app.get('/', function(req, res) {
-    res.end('hello world');
+    res.render('index', {
+        title: 'Timestamp microservice',
+        message: 'Timestamp microservice'
+    });
 });
 
 var port = process.env.PORT || 8080;
