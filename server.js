4a572b54d982a0d051e83f6d397b41b3a67c0659
Thomas Gallin <thomas.gallin@gmail.com>
2016-11-23 15:33:09 +0000

    implemented service that parses the parameter passed in the url and returns timestamps

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var chrono = require('chrono-node');
 
 var app = express();
 
@@ -7,12 +8,38 @@ app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
+// natural date format options
+var options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+};
+
 app.get('/:timestamp', function(req, res, next) {
     var ts = req.params.timestamp;
+    var date;
+    var unixtime = null;
+    var naturaltime = null;
     if (ts) {
-        console.log(ts);
-        res.end(ts);
+        var isnum = /^\d+$/.test(ts);
+        if (isnum) {
+            date = new Date(ts);
+
+        }
+        else {
+            // if ts is not valid, it will return undefined
+            date = chrono.parseDate(ts);
+        }
     }
+    if (date) {
+        unixtime = date.getTime();
+        naturaltime = date.toLocaleString("en-US", options);
+    }
+    var result = {
+        unix: unixtime,
+        natural: naturaltime
+    };
+    res.json(result);
 });
 
 app.get('/', function(req, res) {
